4924ed6b128b0759a84d6c284afe66b0dfe3e72e
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2020-02-24 17:22:28 -0500

    router and model established but blank.

---
+++ api/usersModel.js
@@ -0,0 +1,6 @@
+// import config object (from data folder)
+const db = require('../data/dbConfig.js');
+// export helper functions via the object below as you create them 
+module.exports = {
+    
+}
---
+++ api/usersRouter.js
@@ -0,0 +1,10 @@
+// import express
+const express = require('express');
+// import model 
+const Users = require('./usersModel.js');
+// create the router
+const router = express.Router()
+// begin the endpoints
+
+// export router to server
+module.exports = router
